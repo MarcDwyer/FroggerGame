09e81fd09ff53d8eb09e784b85a4f3c57575961c
Marc Dwyer <envdia@gmail.com>
2018-06-14 18:37:33 -0500

    dl

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function(x, y , speed) {
+const Enemy = function(x, y , speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -19,7 +19,15 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed * dt;
+    // Enemy resets hitting border
+    if (this.x > 505) {
+      this.x = -50;
+    }
 
+    if (player.x == allEnemies[0].x && player.y == allEnemies[0].y) {
+      player.x = 200;
+      player.y = 380;
+    }
 
 };
 
@@ -31,21 +39,67 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-class Player {
-  constructor(x, y, speed) {
+const Player = function(x, y, speed) {
   this.x = x;
   this. y = y;
   this.speed = speed;
   this.sprite = 'images/char-boy.png';
-}
+};
+
+Player.prototype.update = function() {
+// prevent from moving beyond && collisions
+
+if (this.y > 380) {
+     this.y = 380;
+ }
+ if (this.x > 400) {
+     this.x = 400;
+ }
+ if (this.x < 0) {
+     this.x = 0;
+ }
+
+ // Check for player reaching top of canvas and winning the game
+ if (this.y < 0) {
+     this.x = 200;
+     this.y = 380;
+ }
 }
 
-class Update extends Player {
+
+Player.prototype.handleInput = function(control) {
+  switch (control) {
+        case 'left':
+            this.x -= this.speed + 50;
+            break;
+        case 'up':
+            this.y -= this.speed + 30;
+            break;
+        case 'right':
+            this.x += this.speed + 50;
+            break;
+        case 'down':
+            this.y += this.speed + 30;
+            break;
+    }
 
 }
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const enemyPosition = [60, 140, 220];
+const allEnemies =[];
+const player = new Player(200, 380, 50);
+let enemy;
+
+enemyPosition.forEach(function(posY) {
+    enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 512));
+    allEnemies.push(enemy);
+});
 
 
 
